98e7ad4161f5b5952ccb8211d2e60b18bae5c8f8
AntonFor <formanchuk-pru@rambler.ru>
2021-07-09 21:54:31 +0700

    fix-hooks

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -10,211 +10,176 @@ import './app.css';
 
 const classNames = require('classnames');
 
-export default class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+	const [taskData, setTaskData] = useState([]);
+	const [buttonData, setButtonData] = useState([
+		{ class: 'selected', id: 'button-all' },
+    { class: '', id: 'button-active' },
+    { class: '', id: 'button-completed' }
+	]);
+	const [taskEdit, setTaskEdit] = useState({});
 
-    this.state = {
-      taskData: [],
-      buttonData: [
-        { class: 'selected', id: 'button-all' },
-        { class: '', id: 'button-active' },
-        { class: '', id: 'button-completed' },
-      ],
-			taskEdit: {}
-    };
+  const status = {
+    COMPLETED: 'completed',
+    EDITING: 'editing',
+    VIEW: 'view',
+  };
 
-    this.status = {
-      COMPLETED: 'completed',
-      EDITING: 'editing',
-      VIEW: 'view',
-    };
-
-    this.completedItem = (id) => {
-      this.setState(({ taskData }) => {
-        const idx = taskData.findIndex((el) => el.id === id);
-        const checkClass = classNames({
-          completed: !(taskData[idx].class === this.status.COMPLETED),
-        });
-        const oldItem = taskData[idx];
-        const newItem = { ...oldItem, class: checkClass };
-        const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
-				localStorage.setItem('tasks', JSON.stringify(newTaskData));
-				return {
-          taskData: newTaskData,
-        };
+  const completedItem = (id) => {
+		setTaskData((taskData) => {
+			const idx = taskData.findIndex((el) => el.id === id);
+      const checkClass = classNames({
+        completed: !(taskData[idx].class === status.COMPLETED),
       });
-    };
+      const oldItem = taskData[idx];
+      const newItem = { ...oldItem, class: checkClass };
+      const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
+			localStorage.setItem('tasks', JSON.stringify(newTaskData));
+			return taskData = newTaskData;
+		});
+  };
 
-    this.deleteItem = (id) => {
-      this.setState(({ taskData }) => {
-        const newTaskData = taskData.filter((el) => el.id !== id);
-        localStorage.setItem('tasks', JSON.stringify(newTaskData));
-				return {
-          taskData: newTaskData,
-        };
+  const deleteItem = (id) => {
+		setTaskData((taskData) => {
+			const newTaskData = taskData.filter((el) => el.id !== id);
+      localStorage.setItem('tasks', JSON.stringify(newTaskData));
+			return taskData = newTaskData;
+		});
+  };
+
+  const editItem = (id) => {
+		setTaskData((taskData) => {
+			const idx = taskData.findIndex((el) => el.id === id);
+      const checkClass = classNames({
+        editing: !(taskData[idx].class === status.EDITING),
+			});
+      const oldItem = taskData[idx];
+      const newItem = { ...oldItem, class: checkClass };
+      const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
+			return taskData = newTaskData;
+		});
+	};
+
+  const addItem = (text, milsec) => {
+    if (text.length === 0) return;
+    const newItem = createItem(text, milsec);
+    setTaskData((taskData) => {
+			const newTaskData = [...taskData, newItem];
+			localStorage.setItem('tasks', JSON.stringify(newTaskData));
+      return taskData = newTaskData;
+		});
+  };
+
+	const filtrationActiveItem = (id) => {
+		setTaskData((taskData) => {
+			const oldTaskData = [...(taskData)];
+      const newTaskData = oldTaskData.map((el) => {
+        const checkClass = classNames(
+          { view: !(el.class === status.COMPLETED) },
+          { hidden: (el.class === status.COMPLETED) },
+        );
+        const newItem = { ...el, view: checkClass };
+        return newItem;
       });
-    };
+			return taskData = newTaskData;
+		});
+    selectedButton(id);
+  };
 
-    this.editItem = (id) => {
-      this.setState(({ taskData }) => {
-        const idx = taskData.findIndex((el) => el.id === id);
-        const checkClass = classNames({
-          editing: !(taskData[idx].class === this.status.EDITING),
-        });
-        const oldItem = taskData[idx];
-        const newItem = { ...oldItem, class: checkClass };
-        const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
-				return {
-          taskData: newTaskData,
-        };
-      });
-    };
-
-    this.addItem = (text, milsec) => {
-      if (text.length === 0) return;
-      const newItem = this.createItem(text, milsec);
-      this.setState(({ taskData }) => {
-        const newTaskData = [...taskData, newItem];
-				localStorage.setItem('tasks', JSON.stringify(newTaskData));
-        return {
-          taskData: newTaskData,
-        };
-      });
-    };
-
-    this.filtrationActiveItem = (id) => {
-      this.setState(({ taskData }) => {
-        const oldTaskData = [...(taskData)];
-        const newTaskData = oldTaskData.map((el) => {
-          const checkClass = classNames(
-            { view: !(el.class === this.status.COMPLETED) },
-            { hidden: (el.class === this.status.COMPLETED) },
+  const filtrationCompletedItem = (id) => {
+    setTaskData((taskData) => {
+			const oldTaskData = [...(taskData)];
+      const newTaskData = oldTaskData.map((el) => {
+        const checkClass = classNames(
+          { view: (el.class === status.COMPLETED) },
+          { hidden: !(el.class === status.COMPLETED) },
           );
-          const newItem = { ...el, view: checkClass };
-          return newItem;
-        });
-        return {
-          taskData: newTaskData,
-        };
+				const newItem = { ...el, view: checkClass };
+        return newItem;
       });
-      this.selectedButton(id);
-    };
+      return taskData = newTaskData;
+		});
+    selectedButton(id);
+  };
 
-    this.filtrationCompletedItem = (id) => {
-      this.setState(({ taskData }) => {
-        const oldTaskData = [...(taskData)];
-        const newTaskData = oldTaskData.map((el) => {
-          const checkClass = classNames(
-            { view: (el.class === this.status.COMPLETED) },
-            { hidden: !(el.class === this.status.COMPLETED) },
-          );
-          const newItem = { ...el, view: checkClass };
-          return newItem;
-        });
-        return {
-          taskData: newTaskData,
-        };
+  const filtrationAllItem = (id) => {
+		setTaskData((taskData) => {
+			const oldTaskData = [...(taskData)];
+      const newTaskData = oldTaskData.map((el) => {
+      const newItem = { ...el, view: status.VIEW };
+        return newItem;
       });
-      this.selectedButton(id);
-    };
+      return taskData = newTaskData;
+		});
+		selectedButton(id);
+  };
 
-    this.filtrationAllItem = (id) => {
-      this.setState(({ taskData }) => {
-        const oldTaskData = [...(taskData)];
-        const newTaskData = oldTaskData.map((el) => {
-          const newItem = { ...el, view: this.status.VIEW };
-          return newItem;
-        });
-        return {
-          taskData: newTaskData,
-        };
-      });
-      this.selectedButton(id);
-    };
+  const deleteCompletedItems = () => {
+		setTaskData((taskData) => {
+			const oldTaskData = [...(taskData)];
+      const newTaskData = oldTaskData.filter((el) => el.class !== status.COMPLETED);
+      localStorage.setItem('tasks', JSON.stringify(newTaskData));
+			return taskData = newTaskData;
+		});
+  };
 
-    this.deleteCompletedItems = () => {
-      this.setState(({ taskData }) => {
-        const oldTaskData = [...(taskData)];
-        const newTaskData = oldTaskData.filter((el) => el.class !== this.status.COMPLETED);
-        localStorage.setItem('tasks', JSON.stringify(newTaskData));
-				return {
-          taskData: newTaskData,
-        };
-      });
-    };
+  const editTask = (text, id) => {
+		setTaskEdit({ text, id });
+  };
 
-    this.editTask = (text, id) => {
-      this.setState(
-        { taskEdit: { text, id } },
-      );
-    };
+  const eventEdit = ({ key }, id) => {
+    if (key === 'Enter') {
+    const { text } = taskEdit;
+		setTaskData((taskData) => {
+			const idx = taskData.findIndex((el) => el.id === id);
+      const oldItem = taskData[idx];
+      const newItem = { ...oldItem, class: '', description: text };
+      const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
+      localStorage.setItem('tasks', JSON.stringify(newTaskData));
+			return taskData = newTaskData;
+		});
+    }
+  };
 
-    this.eventEdit = ({ key }, id) => {
-      if (key === 'Enter') {
-        const { taskEdit } = this.state;
-        const { text } = taskEdit;
-        this.setState(({ taskData }) => {
-          const idx = taskData.findIndex((el) => el.id === id);
-          const oldItem = taskData[idx];
-          const newItem = { ...oldItem, class: '', description: text };
-          const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
-          localStorage.setItem('tasks', JSON.stringify(newTaskData));
-					return {
-            taskData: newTaskData,
-          };
-        });
-      }
-    };
+	let interval;
+	let delta;
 
-		this.workItemPlay = (id) => {
-			const startTime = Date.now();
-			this.id = setInterval(() => {
-				this.delta = Date.now() - startTime;
-				this.setState(({ taskData }) => {
-					const idx = taskData.findIndex((el) => el.id === id);
-					const oldItem = taskData[idx];
-					const newItem = { ...oldItem, timeWorkTask: this.delta };
-					const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
-					localStorage.setItem('tasks', JSON.stringify(newTaskData));
-					return {
-						taskData: newTaskData,
-					};
-				})
-			}, 1000);
-		}
-
-		this.workItemPause = (id) => {
-			this.setState(({ taskData }) => {
+	const workItemPlay = (id) => {
+		const startTime = Date.now();
+		interval = setInterval(() => {
+			delta = Date.now() - startTime;
+			setTaskData((taskData) => {
 				const idx = taskData.findIndex((el) => el.id === id);
 				const oldItem = taskData[idx];
-				const sumTime = oldItem.sumTimeWorkTask + this.delta;
-				const newItem = { ...oldItem, sumTimeWorkTask: sumTime, timeWorkTask: 0 };
+				const newItem = { ...oldItem, timeWorkTask: delta };
 				const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
 				localStorage.setItem('tasks', JSON.stringify(newTaskData));
-				return {
-					taskData: newTaskData,
-				};
-			})
-			clearInterval(this.id);
-		}
-  }
+				return taskData = newTaskData;
+			});
+		}, 1000);
+	}
 
-	componentDidMount() {
+	const workItemPause = (id) => {
+		setTaskData((taskData) => {
+			const idx = taskData.findIndex((el) => el.id === id);
+			const oldItem = taskData[idx];
+			const sumTime = oldItem.sumTimeWorkTask + delta;
+			const newItem = { ...oldItem, sumTimeWorkTask: sumTime, timeWorkTask: 0 };
+			const newTaskData = [...taskData.slice(0, idx), newItem, ...taskData.slice(idx + 1)];
+			localStorage.setItem('tasks', JSON.stringify(newTaskData));
+			return taskData = newTaskData;
+		});
+		clearInterval(interval);
+	}
+
+	useEffect(() => {
 		const getTasks = localStorage.getItem('tasks');
 		const getTasksPars = JSON.parse(getTasks);
-		this.setState(({ taskData }) => {
-			return {
-				taskData: getTasksPars
-			}
-		})
-  }
+		setTaskData((taskData) => taskData = getTasksPars);
+		return () => {localStorage.setItem('tasks', JSON.stringify(taskData));}
+	}, []);
 
-	componentWillUnmount() {
-    localStorage.setItem('tasks', JSON.stringify(this.state.taskData));
-  }
-
-  createItem(description, milsec) {
+  const createItem = (description, milsec) => {
     return {
       class: '',
       description,
@@ -226,9 +191,9 @@ export default class App extends Component {
     };
   }
 
-	selectedButton(idx) {
-    this.setState(({ buttonData }) => {
-      const oldButtonData = [...(buttonData)];
+	const selectedButton = (idx) => {
+		setButtonData((buttonData) => {
+			const oldButtonData = [...(buttonData)];
       const newButtonData = oldButtonData.map((el) => {
         const checkClass = classNames(
           { selected: (el.id === idx) },
@@ -237,41 +202,36 @@ export default class App extends Component {
         const newItem = { ...el, class: checkClass };
         return newItem;
       });
-      return {
-        buttonData: newButtonData,
-      };
-    });
+      return buttonData = newButtonData;
+		})
   }
 	
-  render() {
-    const { taskData } = this.state;
-    const { taskEdit } = this.state;
-    const { buttonData } = this.state;
-    return (
-      <section className="todoapp">
-        <NewTaskForm addTask={this.addItem} />
-        <section className="main">
-          <TaskList
-            tasks={taskData}
-            completedTask={this.completedItem}
-            deleteTask={this.deleteItem}
-            editTask={this.editItem}
-            changeEditTask={this.editTask}
-            handlingEventEdit={this.eventEdit}
-            value={taskEdit}
-						workTaskPlay={this.workItemPlay}
-						workTaskPause={this.workItemPause}
-          />
-          <Footer
-            tasks={taskData}
-            filtrationActiveTask={this.filtrationActiveItem}
-            filtrationCompletedTask={this.filtrationCompletedItem}
-            filtrationAllTask={this.filtrationAllItem}
-            buttons={buttonData}
-            deleteCompletedTasks={this.deleteCompletedItems}
-          />
-        </section>
+  return (
+    <section className="todoapp">
+      <NewTaskForm addTask={addItem} />
+      <section className="main">
+        <TaskList
+          tasks={taskData}
+          completedTask={completedItem}
+          deleteTask={deleteItem}
+          editTask={editItem}
+          changeEditTask={editTask}
+          handlingEventEdit={eventEdit}
+          value={taskEdit}
+					workTaskPlay={workItemPlay}
+					workTaskPause={workItemPause}
+        />
+        <Footer
+          tasks={taskData}
+          filtrationActiveTask={filtrationActiveItem}
+          filtrationCompletedTask={filtrationCompletedItem}
+          filtrationAllTask={filtrationAllItem}
+          buttons={buttonData}
+          deleteCompletedTasks={deleteCompletedItems}
+        />
       </section>
-    );
-  }
+		</section>
+  );
 }
+
+export default App;
